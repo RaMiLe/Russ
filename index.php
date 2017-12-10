87df35e8267c6b6654ece4e74378ca6e4d5803ff
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-10 22:21:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -36,12 +36,12 @@ name="email" id="email"/></br>
 name="submit" value="Submit" />
 </form>
 <?php
-// DB connection info
+
 $host = "localhost\sqlexpress";
 $user = "user name";
 $pwd = "password";
 $db = "registration";
-// Connect to database.
+
 try {
     $conn = new PDO
 ( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
