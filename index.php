7c4838ffb7f6a6a10bff9d923d83ad5f22dc9255
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-10 22:14:52 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,21 +35,7 @@ name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Submit" />
 </form>
-// DB connection info
-$host = "localhost\sqlexpress";
-$user = "user name";
-$pwd = "password";
-$db = "registration";
-// Connect to database.
-try {
-    $conn = new PDO
-( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute
-( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-}
-catch(Exception $e){
-    die(var_dump($e));
-}
+
 <?php
 
 
