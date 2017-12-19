27ba84ccdcee1b0e782bfd1fff301d13a2cc7355
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 17:45:33 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,18 +1,19 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
 try {
-   $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE TABLE registration_tb(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-   echo "<h3>Таблица города создана!</h3>";
+$conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE registration_too(
+id INT NOT NULL IDENTITY(1,1)
+PRIMARY KEY(id),
+name VARCHAR(30),
+email VARCHAR(30),
+city VARCHAR(30),
+date DATE)";
+$conn->query($sql);
+
+echo "<h3>Таблица создана!</h3>";
 }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+print("Error connecting to SQL Server.");
+die(print_r($e));
 }
