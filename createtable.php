b55b2285b501522453124e8d3108771208081b7c
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 21:02:21 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,6 +1,6 @@
 <?php
 try {
- $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
+  $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE registration_too(
 id INT NOT NULL IDENTITY(1,1)
@@ -11,7 +11,33 @@ city VARCHAR(30),
 date DATE)";
 $conn->query($sql);
 
-echo "<h3>Тики-таки!</h3>";
+echo "<h3>Таблица создана!</h3>";
+}
+catch (PDOException $e) {
+print("Error connecting to SQL Server.");
+die(print_r($e));
+}
+try {
+  $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE city(
+id_city INT NOT NULL IDENTITY(1,1)
+PRIMARY KEY(id),
+city_name VARCHAR(30)";
+$conn->query($sql);
+$sql = "INSERT INTO city (id_city, city_name) VALUES
+(1,"Moskow"),
+(2,"St.Petersburg"),
+(3,"Novosibirsk"),
+(4,"Ecaterenburg"),
+(5,"Novgorod"),
+(6,"Kazan"),
+(7,"Chelyabinsk"),
+(8,"Omsk"),
+(9,"Samara"),
+(10,"Rostov na Donu");
+
+echo "<h3>Таблица города создана!</h3>";
 }
 catch (PDOException $e) {
 print("Error connecting to SQL Server.");
