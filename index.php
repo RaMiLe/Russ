463b7ca974c124f92b0ad7e80c94c4c3a9fbaf5d
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 10:34:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Регистрационная форма</title>
-<link rel ="stylesheet" href ="/style.css">
-<link rel ="stylesheet" href ="/form.css">
+
 </head>
 <body>
 <h1>Зарегистрироваться здесь!</h1>
