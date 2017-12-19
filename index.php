7176d59095c8e34ef894f6d45189657bbd532d2a
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 18:43:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,38 +38,12 @@ name="submit" value="Submit" />
 </form>
 
 <?php
-$dsn = "sqlsrv:server =  tcp:ramil.database.windows.net,1433; Database = Tat", "ramil";
-$username = "ramil";
-$password = "Rosbank1997";
-
 try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if(isset($_POST["clear"])) {
-$sql1 = "DELETE FROM registration_on";
-$conn->query($sql1);
-}
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-
-$conn = null;
-
-?>
-
-<?php
-
-$dsn = "sqlsrv:server =  tcp:ramil.database.windows.net,1433; Database = Tat", "ramil";
-$username = "ramil";
-$password = "Rosbank1997";
-try {
-$conn = new PDO($dsn, $username, $password);
+$conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412danNN");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
+print("Error connecting to SQL Server.");
 die(print_r($e));
 }
 if(!empty($_POST)) {
@@ -77,87 +51,42 @@ try {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
-$gender = $_POST['gender'];
-$age = $_POST['age'];
-$country = $_POST['country'];
-$birthday = $_POST['birthday'];
 
-if ($name == "" || $email == "") {
-echo "<h3>Не заполнены поля name и email.</h3>";
-}
-else {
-$sql_insert ="INSERT INTO registration_on (name, email, date, gender, age, country, birthday) VALUES (?,?,?,?,?,?,?)";
+// Insert data
+$sql_insert =
+"INSERT INTO registration_too (name, email, date)
+VALUES (?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $name);
 $stmt->bindValue(2, $email);
 $stmt->bindValue(3, $date);
-$stmt->bindValue(4, $gender);
-$stmt->bindValue(5, $age);
-$stmt->bindValue(6, $country);
-$stmt->bindValue(7, $birthday);
-$stmt->execute();
 
-echo "<h3>Вы зарегистрировались!</h3>";
-}
+$stmt->execute();
 }
 catch(Exception $e) {
 die(var_dump($e));
 }
-
+echo "<h3>Your're registered!</h3>";
 }
-
-$conn = null;
-
-?>
-
-<?php
-
-$dsn = "sqlsrv:server =  tcp:ramil.database.windows.net,1433; Database = Tat", "ramil";
-$username = "ramil";
-$password = "Rosbank1997";
-try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-
-$sql_select = "SELECT * FROM registration_on";
+$sql_select = "SELECT * FROM registration_too";
 $stmt = $conn->query($sql_select);
-$stmt->execute();
-if(isset($_POST['filter'])) {
-$gender = $_POST['gender'];
-$sql_select = "SELECT * FROM registration_on WHERE gender like :gender";
-$stmt = $conn->prepare($sql_select);
-$stmt->execute(array(':gender'=>$gender.'%'));
-}
 $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) {
-echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<h2>People who are registered:</h2>";
 echo "<table>";
 echo "<tr><th>Name</th>";
 echo "<th>Email</th>";
-echo "<th>Gender</th>";
-echo "<th>Age</th>";
-echo "<th>Country</th>";
-echo "<th>Birthday</th>";
 echo "<th>Date</th></tr>";
+
 foreach($registrants as $registrant) {
-echo "<td>".$registrant['name']."</td>";
+echo "<tr><td>".$registrant['name']."</td>";
 echo "<td>".$registrant['email']."</td>";
-echo "<td>".$registrant['gender']."</td>";
-echo "<td>".$registrant['age']."</td>";
-echo "<td>".$registrant['country']."</td>";
-echo "<td>".$registrant['birthday']."</td>";
 echo "<td>".$registrant['date']."</td></tr>";
 }
 echo "</table>";
+} else {
+echo "<h3>No one is currently registered.</h3>";
 }
-else {
-echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
-}
-
 ?>
-
+</body>
+</html>
