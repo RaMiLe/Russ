84d9262116e666503a8b2e9308db5cd04d15e58e
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 18:11:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ name="submit" value="Submit" />
 
 <?php
 try {
-$conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412danNN");
+$conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
