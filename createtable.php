f437441928d8ff823bdd7fa6798a95dbf45ed3e2
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 20:44:28 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,23 +1,19 @@
 <?php
-// DB connection info
-$host = "localhost\sqlexpress";
-$user = "user name";
-$pwd = "password";
-$db = "registration";
 try {
-    $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "{your_password_here}");
+ $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "CREATE TABLE registration_too(
+id INT NOT NULL IDENTITY(1,1)
+PRIMARY KEY(id),
+name VARCHAR(30),
+email VARCHAR(30),
+city VARCHAR(30),
+date DATE)";
+$conn->query($sql);
+
+echo "<h3>Таблица создана!</h3>";
 }
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
+catch (PDOException $e) {
+print("Error connecting to SQL Server.");
+die(print_r($e));
 }
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
-?>
