5c01a12605488243809825ff68d12cf37193e721
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 10:29:21 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -4,13 +4,16 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat", "ramil", "Rosbank1997");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-    $sql = "CREATE TABLE registration_tbl(
+    $sql = "CREATE TABLE registration_tb(
     id INT NOT NULL IDENTITY(1,1) 
     PRIMARY KEY(id),
     name VARCHAR(30),
     email VARCHAR(30),
+    country VARCHAR(30),
     date DATE)";
     $conn->query($sql);
+
+echo "Все тип-топ!"
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
