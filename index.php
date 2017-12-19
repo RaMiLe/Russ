52e9903cff2037da220593c70f47c118c1dfbd18
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 19:18:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -58,8 +58,8 @@ $date = date("Y-m-d");
 
 // Insert data
 $sql_insert =
-"INSERT INTO registration_too (name, email, date)
-VALUES (?,?,?)";
+"INSERT INTO registration_too (name, email, country, date)
+VALUES (?,?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $name);
 $stmt->bindValue(2, $email);
