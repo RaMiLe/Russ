b17650a4e4e1d5bdf209fffc0a55cf109f9a9034
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 17:54:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,73 +1,49 @@
-<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Регистрационная форма</title>
-<link rel ="stylesheet" href ="/style.css">
-<link rel ="stylesheet" href ="/form.css">
+<Title>Registration Form</Title>
+<style type="text/css">
+body { background-color:
+#fff; border-top: solid 10px #000;
+color: #333; font-size: .85em;
+margin: 20; padding: 20;
+font-family: "Segoe UI",
+Verdana, Helvetica, Sans-Serif;
+}
+h1, h2, h3,{ color: #000;
+margin-bottom: 0; padding-bottom: 0; }
+h1 { font-size: 2em; }
+h2 { font-size: 1.75em; }
+h3 { font-size: 1.2em; }
+table { margin-top: 0.75em; }
+th { font-size: 1.2em;
+text-align: left; border: none; padding-left: 0; }
+td { padding: 0.25em 2em 0.25em 0em;
+border: 0 none; }
+</style>
 </head>
 <body>
-<h1>Зарегистрироваться здесь!</h1>
-<p>Введите свое имя и адрес электронной почты и нажмите кнопку <strong>Отправить</strong> для регистрации.</p>
-<form action="index.php" method="post">
-<div>
-<input type ="text" name ="name" id ="name" placeholder ="Введите ваше имя">
-<input type ="text" name ="email" id ="email" placeholder ="Ваш еmail..">
-<input type ="text" name ="age" id ="age" placeholder ="Ваш возраст..">
-<input type ="text" name ="country" id ="country" placeholder ="Страна">
-<input type ="date" name ="birthday" id ="birthday" placeholder ="Дата рождения">
-<div>
-<input type ="submit" name ="submit" class ="btn" value ="Отправить">
-<input type ="submit" name ="clear" class ="btn" id = "clr" value ="Очистить"></pre>
-</div>
-</div>
-<div>
-<select name ="gender" class ="gen">
-<option value ="">All</option>
-<option value ="Man" <?php if($gender == 'Man'){echo 'selected';}?»Man</option>
-<option value ="Woman" <?php if($gender == 'Woman'){echo 'selected';}?»Woman</option>
-</select>
-<input type="submit" name="filter" class="btn" value="Фильтр">
-</div>
+<h1>Register here!</h1>
+<p>Fill in your name and
+email address, then click <strong>Submit</strong>
+to register.</p>
+<form method="post" action="index.php"
+enctype="multipart/form-data" >
+Name <input type="text"
+name="name" id="name"/></br>
+Email <input type="text"
+name="email" id="email"/></br>
+
+<input type="submit"
+name="submit" value="Submit" />
 </form>
-</body>
-</html>
 
 <?php
-$dsn = "sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat";
-$username = "ramil";
-$password = "Rosbank1997";
-
 try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-if(isset($_POST["clear"])) {
-$sql1 = "DELETE FROM registration_on";
-$conn->query($sql1);
-}
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-
-$conn = null;
-
-?>
-
-<?php
-
-$dsn = "sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat";
-$username = "ramil";
-$password = "Rosbank1997";
-
-
-try {
-$conn = new PDO($dsn, $username, $password);
+$conn = new PDO("sqlsrv:server = tcp:caramba878.database.windows.net,1433; Database = Caramba878", "Caramba878", "535412danNN");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
+print("Error connecting to SQL Server.");
 die(print_r($e));
 }
 if(!empty($_POST)) {
@@ -75,88 +51,42 @@ try {
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = date("Y-m-d");
-$gender = $_POST['gender'];
-$age = $_POST['age'];
-$country = $_POST['country'];
-$birthday = $_POST['birthday'];
 
-if ($name == "" || $email == "") {
-echo "<h3>Не заполнены поля name и email.</h3>";
-}
-else {
-$sql_insert ="INSERT INTO registration_on (name, email, date, gender, age, country, birthday) VALUES (?,?,?,?,?,?,?)";
+// Insert data
+$sql_insert =
+"INSERT INTO registration_too (name, email, date)
+VALUES (?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $name);
 $stmt->bindValue(2, $email);
 $stmt->bindValue(3, $date);
-$stmt->bindValue(4, $gender);
-$stmt->bindValue(5, $age);
-$stmt->bindValue(6, $country);
-$stmt->bindValue(7, $birthday);
-$stmt->execute();
 
-echo "<h3>Вы зарегистрировались!</h3>";
-}
+$stmt->execute();
 }
 catch(Exception $e) {
 die(var_dump($e));
 }
-
+echo "<h3>Your're registered!</h3>";
 }
-
-$conn = null;
-
-?>
-
-<?php
-
-$dsn = "sqlsrv:server = tcp:ramil.database.windows.net,1433; Database = Tat";
-$username = "ramil";
-$password = "Rosbank1997";
-
-
-try {
-$conn = new PDO($dsn, $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-print("Ошибка подключения к SQL Server.");
-die(print_r($e));
-}
-
-$sql_select = "SELECT * FROM registration_on";
+$sql_select = "SELECT * FROM registration_too";
 $stmt = $conn->query($sql_select);
-$stmt->execute();
-if(isset($_POST['filter'])) {
-$gender = $_POST['gender'];
-$sql_select = "SELECT * FROM registration_on WHERE gender like :gender";
-$stmt = $conn->prepare($sql_select);
-$stmt->execute(array(':gender'=>$gender.'%'));
-}
 $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) {
-echo "<h2>Люди, которые зарегистрированы:</h2>";
+echo "<h2>People who are registered:</h2>";
 echo "<table>";
 echo "<tr><th>Name</th>";
 echo "<th>Email</th>";
-echo "<th>Gender</th>";
-echo "<th>Age</th>";
-echo "<th>Country</th>";
-echo "<th>Birthday</th>";
 echo "<th>Date</th></tr>";
+
 foreach($registrants as $registrant) {
-echo "<td>".$registrant['name']."</td>";
+echo "<tr><td>".$registrant['name']."</td>";
 echo "<td>".$registrant['email']."</td>";
-echo "<td>".$registrant['gender']."</td>";
-echo "<td>".$registrant['age']."</td>";
-echo "<td>".$registrant['country']."</td>";
-echo "<td>".$registrant['birthday']."</td>";
 echo "<td>".$registrant['date']."</td></tr>";
 }
 echo "</table>";
+} else {
+echo "<h3>No one is currently registered.</h3>";
 }
-else {
-echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
-}
-
 ?>
+</body>
+</html>
