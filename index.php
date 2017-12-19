acc32d470472f1d16cf3bae356043194d660beec
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 18:16:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -78,6 +78,7 @@ if(count($registrants) > 0) {
 echo "<h2>People who are registered:</h2>";
 echo "<table>";
 echo "<tr><th>Name</th>";
+  echo "<tr><th>Name</th>";
 echo "<th>Email</th>";
 echo "<th>Date</th></tr>";
 
