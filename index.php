2f068c739533ab216b2e95b748737daa6ac7c3e6
RaMiLe <32164111+RaMiLe@users.noreply.github.com>
2017-12-19 19:08:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ Name <input type="text"
 name="name" id="name"/></br>
 Email <input type="text"
 name="email" id="email"/></br>
-    Email <input type="text"
+   Country <input type="text"
 name="country" id="country"/></br>
 
 <input type="submit"
